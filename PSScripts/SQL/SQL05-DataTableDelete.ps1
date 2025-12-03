a83c8564b2f5aa424a1d3233296dd2ef6f05ab53
distcode <office@dist.at>
2025-12-03 11:47:13 +0100

    SQL Script updated

---
+++ PSScripts/SQL/SQL05-DataTableDelete.ps1
@@ -0,0 +1,19 @@
+
+$connectionstring = 'Server=tcp:maps2512sqlsrvfp.database.windows.net,1433;Initial Catalog=DB01;Persist Security Info=False;User ID=sqladmin;Password=Pa55w.rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;'
+
+$myConn = New-Object System.Data.SqlClient.SqlConnection
+$myConn.ConnectionString = $connectionstring
+$myConn.Open()
+# $myConn.State
+
+$commandstring = "Select * From SalesLT.Customer Where Lastname='Lator'"
+
+$myDataAdapter = New-Object -TypeName System.Data.SqlClient.SqlDataAdapter($commandstring, $myConn)
+$myCommandBuilder = New-Object System.Data.SqlClient.SqlCommandBuilder($myDataAdapter)
+
+$myDataTable = New-Object -TypeName System.Data.DataTable
+$rowcount = $myDataAdapter.Fill($myDataTable)
+
+$myDataTable.Rows[0].Delete()
+
+$myDataAdapter.Update($myDataTable)
